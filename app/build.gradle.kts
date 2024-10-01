c954abc2efe7fcc4c2c2fc569329eea204fde8ae
jp247 <jp247@students.waikato.ac.nz>
2024-10-02 02:06:43 +1300

    Map View UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.preference)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
