2684384a3e014e72fb61c4f29585db32c9ad53f2
jp247 <jp247@students.waikato.ac.nz>
2024-10-16 00:53:16 +1300

    Settings with dark mode option and other buttons

--- app/src/main/java/com/example/hodofiles/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/hodofiles/ui/settings/SettingsFragment.java
@@ -2,7 +2,10 @@ package com.example.hodofiles.ui.settings;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -16,5 +19,20 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //Handle theme switching
+        SwitchCompat themeSwitch = view.findViewById(R.id.switch_theme);
+        themeSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
+        });
     }
 }
