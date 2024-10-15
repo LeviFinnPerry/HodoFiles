d35261a12718cc4ba9b2a6169a24d7ed0fbc1550
jp247 <jp247@students.waikato.ac.nz>
2024-10-16 01:55:04 +1300

    Settings with function dark mode

--- app/src/main/java/com/example/hodofiles/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/hodofiles/ui/settings/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.example.hodofiles.ui.settings;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,6 +8,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +17,8 @@ import android.view.ViewGroup;
 import com.example.hodofiles.R;
 
 public class SettingsFragment extends Fragment {
+    private SharedPreferences sharedPreferences;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -25,14 +29,31 @@ public class SettingsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        //Handle theme switching
+        //Get the current preference
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+        //Handle current theme with switch syncing
         SwitchCompat themeSwitch = view.findViewById(R.id.switch_theme);
+        boolean isDarkMode = sharedPreferences.getBoolean("dark_mode", false);
+        themeSwitch.setChecked(isDarkMode);
+
         themeSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
             if (isChecked) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                saveThemePreference(true);
+                requireActivity().recreate();
             } else {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                saveThemePreference(false);
+                requireActivity().recreate();
             }
         });
+    }
+
+    // Save the theme preference in SharedPreferences
+    private void saveThemePreference(boolean isDarkMode) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("dark_mode", isDarkMode);
+        editor.apply();
     }
 }
